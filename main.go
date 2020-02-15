7c65d099127ac6ca558775f2a0581b6e7a465cb2
xudong12 <xudong12@staff.weibo.com>
2020-02-15 11:58:33 +0800

    添加新功能

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
-
+var  num int = 1
 func main() {
 	// 创建一个echo实例
 	// 不同于iris、gin 是直接使用New
@@ -32,5 +33,7 @@ func main() {
 // 路由handle提出来了而已
 // 匿名函数方式 不重要
 func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "感恩相遇，未来可期，我的联系方式：0001 0000 0110 0111 1000 1001 0010 0101 0000 0011")
+	num = num +1
+	 var  contactStr ="感恩相遇，未来可期，你是第"+strconv.Itoa(num)+"位获取我的联系方式：0001 0000 0110 0111 1000 1001 0010 0101 0000 0011"
+	return c.String(http.StatusOK, contactStr)
 }
